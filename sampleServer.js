c2f6ed7b697249cbf381f424a4ef56e5557fd8ed
hisham dulfacker <dulfackerhisham@gmail.com>
2024-08-05 20:13:12 +0530

    html response

--- sampleServer.js
+++ sampleServer.js
@@ -1,14 +1,16 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    const user = {
-        firstName: "John",
-        lastName: "Doe",
-    }
+    // const user = {
+    //     firstName: "John",
+    //     lastName: "Doe",
+    // }
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(user));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const html = fs.readFileSync("./sample.html", "utf-8");
+    res.end(html);
 });
 
 server.listen(3000, () => {
