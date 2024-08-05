4629930a06d15449c55c12da6cccd2a69e72239a
hisham dulfacker <dulfackerhisham@gmail.com>
2024-08-05 20:00:04 +0530

    JSON response

--- sampleServer.js
+++ sampleServer.js
@@ -1,11 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("hello world!");
+
+    const user = {
+        firstName: "John",
+        lastName: "Doe",
+    }
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(user));
 });
 
 server.listen(3000, () => {
     console.log("server running on port 3000");
-    
 })
